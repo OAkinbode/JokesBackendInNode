56b9d8129766617ed910ea2033d0610496c91d4b
OAkinbode <olusola.akinbode@ucalgary.ca>
2023-05-09 22:04:43 -0600

    changes made

---
+++ modules.js
@@ -0,0 +1,8 @@
+const ppl = require("./people");
+const { people, ages } = require("./people");
+
+// console.log(ppl.ages);
+// console.log(people, ages);
+
+const os = require("os");
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ["Sola", "Jummy", "Lola", "Temmy"];
+const ages = [12, 14, 9, 27];
+
+// console.log(people);
+
+module.exports = {
+  people: people,
+  ages: ages,
+};
